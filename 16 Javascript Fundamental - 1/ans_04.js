4258792a054a64004870870832286d583f03ede8
Abhinav Kumar <abhinavkumar2369@outlook.com>
2024-08-09 23:59:19 +0530

    ✅ Done

---
+++ 16 Javascript Fundamental - 1/ans_04.js
@@ -0,0 +1,7 @@
+let numberOfChilds = 2;
+let numberOfAdults = 1;
+let numberOfSeniors = 1;
+
+total_price = numberOfChilds*100 + numberOfAdults*150 + numberOfSeniors*120;
+
+console.log("The total ticket price is "+ total_price)
