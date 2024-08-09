6428444f631fa27a6b9cbcfa55e834c54bcbed0c
Abhinav Kumar <abhinavkumar2369@outlook.com>
2024-08-09 23:58:53 +0530

    ✅ Done

---
+++ 16 Javascript Fundamental - 1/ans_02.js
@@ -0,0 +1,9 @@
+let username = "admin"
+let password = 12345
+
+if (username==="admin" && password===12345){
+    console.log("Login Successful")
+}
+else{
+    console.log("Invalid Credentials")
+}
