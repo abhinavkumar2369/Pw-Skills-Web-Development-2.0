180f48b91a9e354740fcb6fc22a4b7d4bf6065db
Abhinav Kumar <abhinavkumar2369@outlook.com>
2024-08-09 23:59:28 +0530

    ✅ Done

---
+++ 16 Javascript Fundamental - 1/ans_05.js
@@ -0,0 +1,13 @@
+
+switch("standard"){
+    case "standard":
+        console.log("3-5 days");
+        break
+    case "express":
+        console.log("1-2 days");
+        break
+    case "overnight":
+        console.log("Delivery would be next day");
+        break
+}
+
