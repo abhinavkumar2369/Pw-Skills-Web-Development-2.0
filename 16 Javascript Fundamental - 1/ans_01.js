3247343bb91f6150601ed733a732c113ced49eb5
Abhinav Kumar <abhinavkumar2369@outlook.com>
2024-08-09 23:57:50 +0530

    ✅  Done

---
+++ 16 Javascript Fundamental - 1/ans_01.js
@@ -0,0 +1,6 @@
+let total_value = 2000
+let discount_percentage = 20
+
+let discount_value = total_value - (total_value*(discount_percentage/100))
+
+console.log("The final price after the discount is " + discount_value)
