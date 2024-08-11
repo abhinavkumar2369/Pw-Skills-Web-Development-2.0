c2c8c9023674200cc166386053366a9ba09b28a1
Abhinav Kumar <abhinavkumar2369@outlook.com>
2024-08-11 21:57:46 +0530

    ✅ Done

---
+++ 18 JavaScript Fundamental - 3/ans_05.js
@@ -0,0 +1,10 @@
+// Function Declaration
+function areaOfCicrlce(radius){
+    let area=Math.PI*(radius**2)
+    console.log(`Area of Circle is ${area}`)
+}
+
+// Function Call
+areaOfCicrlce(1)
+areaOfCicrlce(2)
+areaOfCicrlce(3)
