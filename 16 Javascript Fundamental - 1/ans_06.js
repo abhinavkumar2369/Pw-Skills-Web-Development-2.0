397b7e60df8dcdef6e75bd8bbfd614d883e164ce
Abhinav Kumar <abhinavkumar2369@outlook.com>
2024-08-14 15:53:00 +0530

    ✅ Done

---
+++ 16 Javascript Fundamental - 1/ans_06.js
@@ -0,0 +1,13 @@
+let name = "Mithun"
+let email = "mithun.s@pw.live"
+let age = "21"
+
+if ((typeof name ) != "string"){
+    console.log("Name Should be a String")
+}
+if ((typeof email ) != "string"){
+    console.log("Email Should be a String")
+}
+if ((typeof age ) != "number"){
+    console.log("Age Should be A Number")
+}
