7b7383a813eba60230bb16125ce2266b8a09e012
Abhinav Kumar <abhinavkumar2369@outlook.com>
2024-08-14 15:51:57 +0530

    ✅ Done

---
+++ 17 Javascript Fundamental - 2/ans_06.js
@@ -0,0 +1,10 @@
+const productDetails = {
+    name:"Apple 2020 MacBook Air Laptop",
+    price: 82000,
+    color:"Grey",
+    hardDisk: "256 GB"
+}
+
+console.log('Below are the products details.')
+
+console.log(` Name : ${productDetails.name} \n Price : ${productDetails.price} \n Color : ${productDetails.price} \n HardDisk : ${productDetails.hardDisk}`)
