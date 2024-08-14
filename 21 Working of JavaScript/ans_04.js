69a8fd8a22507ff8a9ab6f067cd51472c0e6b04e
Abhinav Kumar <abhinavkumar2369@outlook.com>
2024-08-14 19:18:38 +0530

    ✅ Done

---
+++ 21 Working of JavaScript/ans_04.js
@@ -0,0 +1,27 @@
+{
+    try {
+        console.log(a);
+    } catch (e) {
+        console.log(e.message);
+    }
+
+
+    let a = 10;
+    console.log(a);
+
+
+    console.log(b);
+    var b = 20;
+    console.log(b);
+
+
+    try {
+        console.log(c);
+    } catch (e) {
+        console.log(e.message);
+    }
+
+    
+    const c = 30;
+    console.log(c);
+}
